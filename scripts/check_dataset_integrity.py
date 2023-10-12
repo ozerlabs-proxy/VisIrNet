56184f1092532dcc6a821d42b4baf62e0c6d131a
nalain-ozerlab <alain.ndigande@ozu.edu.tr>
2023-10-13 02:15:39 +0300

    fixing and checking dataset sanity

--- scripts/check_dataset_integrity.py
+++ scripts/check_dataset_integrity.py
@@ -44,10 +44,11 @@ dataset="SkyData"
 BATCH_SIZE = 1
 SHUFFLE_BUFFER_SIZE = 100
 
-for dataset in ["SkyData","GoogleEarth","GoogleMap","MSCOCO","VEDAI"]:
+# for dataset in ["MSCOCO"]:
+for dataset in ["MSCOCO","VEDAI","SkyData","GoogleEarth","GoogleMap"]:
 
     print("**dataset: ", dataset)
-    train_dataloader,test_dataloader = data_setup.create_dataloaders(dataset=dataset, 
+    train_dataloader, test_dataloader = data_setup.create_dataloaders(dataset=dataset, 
                                                                     BATCH_SIZE=BATCH_SIZE,
                                                                     SHUFFLE_BUFFER_SIZE=100
                                                                     )
@@ -101,12 +102,44 @@ for dataset in ["SkyData","GoogleEarth","GoogleMap","MSCOCO","VEDAI"]:
             if not _is_invertibles:
                 dataset_info[split]["nonInvertibles"].append(str(_instances.numpy()[0]))
                 
-            warped_images, _transformed_have_nans = DatasetTools._transformed_images(input_images, 
-                                                                                    homography_matrices)
+            try:
+                warped_images, _transformed_have_nans = DatasetTools._transformed_images(input_images,homography_matrices)
+            except:
+                # log all shapes
+                print("*"*50 + " _instances " + "*"*50)
+                print(_instances)
+                print(f"input_images.shape: {input_images.shape}")
+                print(f"template_images.shape: {template_images.shape}")
+                print(f"labels.shape: {labels.shape}")
+                print(f"homography_matrices.shape: {homography_matrices.shape}")
+                print(f"inverse_homography_matrices.shape: {inverse_homography_matrices.shape}")
+                
+                
+                # now log types
+                print("*"*50 + " types " + "*"*50)
+                print(f"input_images.dtype: {type(input_images)}")
+                print(f"template_images.dtype: {type(template_images)}")
+                print(f"labels.dtype: {type(labels)}")
+                print(f"homography_matrices.dtype: {type(homography_matrices)}")
+                print(f"inverse_homography_matrices.dtype: {type(inverse_homography_matrices)}")
+                
+                # now do they have nans
+                print("*"*50 + " nans " + "*"*50)
+                print(f"np.isnan(input_images).any(): {np.isnan(input_images).any()}")
+                print(f"np.isnan(template_images).any(): {np.isnan(template_images).any()}")
+                print(f"np.isnan(labels).any(): {np.isnan(labels).any()}")
+                print(f"np.isnan(homography_matrices).any(): {np.isnan(homography_matrices).any()}")
+                print(f"np.isnan(inverse_homography_matrices).any(): {np.isnan(inverse_homography_matrices).any()}")
+                
+                continue                
+            
             # check for nans in transformed images
             if _transformed_have_nans:
                 dataset_info[split]["transformed_have_NaNs"].append(str(_instances.numpy()[0]))
-                
+        
+        dataset_info[split]["seen_shapes_inputs"] = list(dataset_info[split]["seen_shapes_inputs"] )
+        dataset_info[split]["seen_shapes_templates"] = list(dataset_info[split]["seen_shapes_templates"])    
+        
     #save dataset info
     common_utils.save_json(dataset_info, 
                             f"resources/datasets_logs/",
